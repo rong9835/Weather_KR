c9936cb72995a2c7cec8cac9ba18bbccb87c9067
박초롱 <rong9835@naver.com>
2024-11-14 22:56:07 +0900

    feat:헤더의 기본구조작성,로고와 임시 검색창

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Cloud, DivideSquare } from "lucide-react";
 
 function HomePage() {
-  return <div className="min-h-screen bg-black text-white">home</div>;
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <header className="w-full h-20 flex items-center p-6 gap-6">
+        <div className="flex items-center p-6 gap-6">
+          <Cloud className="w-10 h-10" />
+          <div className="test-2xl">Weather.io</div>
+          <div>검색입력창</div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default HomePage;
